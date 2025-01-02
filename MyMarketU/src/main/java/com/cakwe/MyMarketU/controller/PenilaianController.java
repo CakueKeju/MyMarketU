04326c652d0593f0ba68254ea333f03d767198e4
qil <148635287+qilelric@users.noreply.github.com>
2025-01-02 15:50:52 +0700

    add PenilaianController

---
+++ MyMarketU/src/main/java/com/cakwe/MyMarketU/controller/PenilaianController.java
@@ -0,0 +1,41 @@
+package com.cakwe.MyMarketU.controller;
+
+import com.cakwe.MyMarketU.model.Penilaian;
+import com.cakwe.MyMarketU.repository.PenilaianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/penilaian")
+public class PenilaianController {
+
+    @Autowired
+    private PenilaianRepository penilaianRepository;
+
+    @PostMapping("/add")
+    public ResponseEntity<Penilaian> addPenilaian(@RequestBody Penilaian penilaian) {
+        Penilaian savedPenilaian = penilaianRepository.save(penilaian);
+        return ResponseEntity.ok(savedPenilaian);
+    }
+
+    @GetMapping("/produk/{idProduk}")
+    public ResponseEntity<List<Penilaian>> getByProduk(@PathVariable int idProduk) {
+        List<Penilaian> penilaianList = penilaianRepository.findByIdProduk(idProduk);
+        return ResponseEntity.ok(penilaianList);
+    }
+
+    @GetMapping("/pelanggan/{idPelanggan}")
+    public ResponseEntity<List<Penilaian>> getByPelanggan(@PathVariable int idPelanggan) {
+        List<Penilaian> penilaianList = penilaianRepository.findByIdPelanggan(idPelanggan);
+        return ResponseEntity.ok(penilaianList);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePenilaian(@PathVariable int id) {
+        penilaianRepository.deleteById(id);
+        return ResponseEntity.ok("Penilaian berhasil dihapus.");
+    }
+}
